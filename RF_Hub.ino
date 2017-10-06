d6b3f85d65bd250e1b0aa961ab01d3829b98160d
tfleech <32581463+tfleech@users.noreply.github.com>
2017-10-06 17:29:25 -0400

    Added Arduino files to send and receive messages

---
+++ RF_Hub.ino
@@ -0,0 +1,70 @@
+#include <SPI.h>
+#include "nRF24L01.h"
+#include "RF24.h"
+#include "printf.h"
+
+RF24 radio(9,10);
+
+const uint64_t pipes[2] = { 0xF0F0F0F0E1LL, 0xF0F0F0F0D2LL };
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(57600);
+  Serial.println("Serial Open");
+  printf_begin();
+  //printf("\n\rRF24/examples/GettingStarted/\n\r");
+
+  radio.begin();
+  radio.setRetries(15,15);
+
+  Serial.println("radio started");
+
+  radio.openWritingPipe(pipes[0]);
+  radio.openReadingPipe(1,pipes[1]);
+
+  radio.stopListening();
+  radio.printDetails();
+  Serial.println("finished Setup");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (Serial.available()){
+    int Q = Serial.read();
+    RF_Request(Q);
+  }
+}
+
+void RF_Request(unsigned long query){
+  unsigned long time = millis();
+  //printf("Now sending %lu...",query);
+  bool ok = radio.write( &query, sizeof(unsigned long));
+
+  /*if(ok){
+    printf("ok...");
+  }else{
+    printf("failed.\n\r");
+  }*/
+
+  radio.startListening();
+
+  unsigned long started_waiting_at = millis();
+  bool timeout = false;
+  while(!radio.available() && !timeout){
+    if (millis() - started_waiting_at > 200){
+      timeout = true;
+    }
+  }
+
+  if (timeout){
+    Serial.println("timeout");
+  }else{
+    unsigned long response;
+    radio.read( &response, sizeof(unsigned long));
+
+    Serial.println(response);
+  }
+
+  radio.stopListening();
+}
+
---
+++ RF_Sensor.ino
@@ -0,0 +1,98 @@
+#include <SPI.h>
+#include "nRF24L01.h"
+#include "RF24.h"
+#include "printf.h"
+
+RF24 radio(9,10);
+int lightPin=3;
+
+const int analogIn = A0;
+int mVperAmp = 100;
+int RawValue = 0;
+int ACSoffset = 2500;
+double Voltage = 0;
+double Amps = 0;
+
+const uint64_t pipes[2] = { 0xF0F0F0F0E1LL, 0xF0F0F0F0D2LL };
+
+//typedef enum { role_ping_out = 1, role_pong_back } role_e;
+//const char* role_friendly_name[] = { "invalid", "Ping out", "Pong back"};
+//role_e role = role_pong_back;
+
+double getSwitchCurrent() {
+  RawValue = analogRead(analogIn);
+  Voltage = (RawValue / 1024.0)*5000;
+  Amps = ((Voltage - ACSoffset) / mVperAmp);
+  return Amps;
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(4, OUTPUT);
+  
+  Serial.begin(57600);
+  printf_begin();
+  printf("\n\rRF24/examples/GettingStarted/\n\r");
+
+  radio.begin();
+  radio.setRetries(15,15);
+
+  radio.openWritingPipe(pipes[1]);
+  radio.openReadingPipe(1,pipes[0]);
+
+  radio.startListening();
+
+  radio.printDetails();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (radio.available()){
+    Serial.println("Got Something");
+    unsigned long query;
+    unsigned long resp;
+    bool done = false;
+    while(!done){
+      done = radio.read( &query, sizeof(unsigned long));
+
+      printf("Got query %lu...",query);
+
+      delay(20);
+    }
+
+    radio.stopListening();
+
+    //resp = analogRead(4);
+
+    // LightON
+    if (query == 49){
+      //if (getSwitchCurrent() < 1){
+        digitalWrite(4, LOW);
+        Serial.println("Turning on light");
+      //}
+      resp = 1;
+      
+    // Light OFF
+    } else if (query == 50){
+      //if (getSwitchCurrent() > 1){
+        digitalWrite(4, HIGH);
+      //}
+      resp = 1;
+
+    // Light Status
+    } else if (query == 51){
+      if (getSwitchCurrent()>1){
+        resp = 2;
+      }else{
+        resp = 3;
+      }
+    } else if (query == 52){
+      resp = getSwitchCurrent();
+    }
+    
+    radio.write(&resp, sizeof(unsigned long));
+    printf("Sent response.\n\r");
+
+    radio.startListening();
+  }
+}
